08bb5eef8f2066cb427c9f4ee7da471002558762
illuminate3 <carsten.sapporo@gmail.com>
2015-09-10 22:13:09 -0500

    dev 19.1.2

--- Database/Seeds/CoreDatabaseSeeder.php
+++ Database/Seeds/CoreDatabaseSeeder.php
@@ -18,11 +18,11 @@ class CoreDatabaseSeeder extends Seeder
 	{
 		Model::unguard();
 
-//		$this->call('App\Modules\Core\Database\Seeds\ModulePermissionsSeeder');
+		$this->call('App\Modules\Core\Database\Seeds\ModulePermissionsSeeder');
 		$this->call('App\Modules\Core\Database\Seeds\ModuleLinksSeeder');
-// 		$this->call('App\Modules\Core\Database\Seeds\LocaleTableSeeder');
-// 		$this->call('App\Modules\Core\Database\Seeds\SettingsTableSeeder');
-// 		$this->call('App\Modules\Core\Database\Seeds\StatusesSeeder');
+		$this->call('App\Modules\Core\Database\Seeds\LocaleTableSeeder');
+		$this->call('App\Modules\Core\Database\Seeds\SettingsTableSeeder');
+		$this->call('App\Modules\Core\Database\Seeds\StatusesSeeder');
 	}
 
 }
