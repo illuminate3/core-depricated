d64e1feb5f24d4c1ed534e2d046ad77e9ba249c1
illuminate3 <carsten.sapporo@gmail.com>
2016-01-29 11:24:45 -0600

    dev 20.1.2

--- Providers/CoreServiceProvider.php
+++ Providers/CoreServiceProvider.php
@@ -82,6 +82,7 @@ class CoreServiceProvider extends ServiceProvider
 
 		$app->register('App\Modules\Core\Providers\ViewComposerServiceProvider');
 		$app->register('anlutro\LaravelSettings\ServiceProvider');
+		$app->register('Cviebrock\EloquentSluggable\SluggableServiceProvider');
 
 	}
 
