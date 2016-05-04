3ad814356a932cb6e4b806b66107418773b346a2
illuminate3 <carsten.sapporo@gmail.com>
2016-05-04 11:05:08 -0500

    dev 20.4.6.1

--- Providers/CoreServiceProvider.php
+++ Providers/CoreServiceProvider.php
@@ -90,10 +90,10 @@ class CoreServiceProvider extends ServiceProvider
 			'Caffeinated\SEO\Facades\Metadata'
 		);
 
-		AliasLoader::getInstance()->alias(
-			'Breadcrumbs',
-			'DaveJamesMiller\Breadcrumbs\Facade'
-		);
+// 		AliasLoader::getInstance()->alias(
+// 			'Breadcrumbs',
+// 			'DaveJamesMiller\Breadcrumbs\Facade'
+// 		);
 
 /*
 		AliasLoader::getInstance()->alias(
@@ -119,7 +119,7 @@ class CoreServiceProvider extends ServiceProvider
 		$app->register('anlutro\LaravelSettings\ServiceProvider');
 		$app->register('Cviebrock\EloquentSluggable\SluggableServiceProvider');
 		$app->register('ShvetsGroup\LaravelEmailDatabaseLog\LaravelEmailDatabaseLogServiceProvider');
-		$app->register('DaveJamesMiller\Breadcrumbs\ServiceProvider');
+//		$app->register('DaveJamesMiller\Breadcrumbs\ServiceProvider');
 
 // Register Middleware
 		$kernel = $this->app->make('Illuminate\Contracts\Http\Kernel');
