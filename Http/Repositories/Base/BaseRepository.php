a22bdc61396189b2b3d19e894d744230fdbb266e
illuminate3 <carsten.sapporo@gmail.com>
2016-05-16 22:39:11 -0500

    dev 20.5.1

--- Http/Repositories/Base/BaseRepository.php
+++ Http/Repositories/Base/BaseRepository.php
@@ -2,86 +2,90 @@
 
 namespace App\Modules\Core\Repositories\Base;
 
+
 /**
  * Interface CoreRepository
  * @package Modules\Core\Repositories
  */
 interface BaseRepository
 {
-    /**
-     * @param  int    $id
-     * @return $model
-     */
-    public function find($id);
 
-    /**
-     * Return a collection of all elements of the resource
-     * @return mixed
-     */
-    public function all();
+	/**
+	 * @param  int    $id
+	 * @return $model
+	 */
+	public function find($id);
 
-    /**
-     * Create a resource
-     * @param $data
-     * @return mixed
-     */
-    public function create($data);
+	/**
+	 * Return a collection of all elements of the resource
+	 * @return mixed
+	 */
+	public function all();
 
-    /**
-     * Update a resource
-     * @param $model
-     * @param  array $data
-     * @return mixed
-     */
-    public function update($model, $data);
+	/**
+	 * Create a resource
+	 * @param $data
+	 * @return mixed
+	 */
+	public function create($data);
 
-    /**
-     * Destroy a resource
-     * @param $model
-     * @return mixed
-     */
-    public function destroy($model);
+	/**
+	 * Update a resource
+	 * @param $model
+	 * @param  array $data
+	 * @return mixed
+	 */
+	public function update($model, $data);
 
-    /**
-     * Return resources translated in the given language
-     * @param $lang
-     * @return object
-     */
-    public function allTranslatedIn($lang);
+	/**
+	 * Destroy a resource
+	 * @param $model
+	 * @return mixed
+	 */
+	public function destroy($model);
 
-    /**
-     * Find a resource by the given slug
-     * @param  int    $slug
-     * @return object
-     */
-    public function findBySlug($slug);
+	/**
+	 * Return resources translated in the given language
+	 * @param $lang
+	 * @return object
+	 */
+	public function allTranslatedIn($lang);
 
-    /**
-     * Find a resource by an array of attributes
-     * @param  array  $attributes
-     * @return object
-     */
-    public function findByAttributes(array $attributes);
+	/**
+	 * Find a resource by the given slug
+	 * @param  int    $slug
+	 * @return object
+	 */
+	public function findBySlug($slug);
 
-    /**
-     * Return a collection of elements who's ids match
-     * @param array $ids
-     * @return mixed
-     */
-    public function findByMany(array $ids);
+	/**
+	 * Find a resource by an array of attributes
+	 * @param  array  $attributes
+	 * @return object
+	 */
+	public function findByAttributes(array $attributes);
 
-    /**
-     * Get resources by an array of attributes
-     * @param array $attributes
-     * @param null|string $orderBy
-     * @param string $sortOrder
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
-    public function getByAttributes(array $attributes, $orderBy = null, $sortOrder = 'asc');
+	/**
+	 * Return a collection of elements who's ids match
+	 * @param array $ids
+	 * @return mixed
+	 */
+	public function findByMany(array $ids);
 
-    /**
-     * Clear the cache for this Repositories' Entity
-     * @return bool
-     */
-    public function clearCache();
+	/**
+	 * Get resources by an array of attributes
+	 * @param array $attributes
+	 * @param null|string $orderBy
+	 * @param string $sortOrder
+	 * @return \Illuminate\Database\Eloquent\Collection
+	 */
+	public function getByAttributes(array $attributes, $orderBy = null, $sortOrder = 'asc');
+
+	/**
+	 * Clear the cache for this Repositories' Entity
+	 * @return bool
+	 */
+	public function clearCache();
+
+
 }
